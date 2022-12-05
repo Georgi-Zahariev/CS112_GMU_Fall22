edc6f45c9e8f2a94a738b21890cd8812d8887388
Georgi-Zahariev <74987305+Georgi-Zahariev@users.noreply.github.com>
2022-12-05 14:39:15 -0500

    Create gzaharie_213_Lab1.py

---
+++ CS112_GMU/Labs/gzaharie_213_Lab1.py
@@ -0,0 +1,8 @@
+v1=int(input("Please enter the value of v1 in m/s: "))
+v2=int(input("Please enter the value of v2 in m/s: "))
+t1=int(input("Please enter the value of t1 in s: "))
+t2=int(input("Please enter the value of t2 in s: "))
+
+acceleration = (v2-v1)/t2
+
+print("The acceration of Sarah is:", int(acceleration) , "meter per second square")
